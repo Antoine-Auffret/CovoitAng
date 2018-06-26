2af7af7bef84cac0965be9463c778670dbade2f8
Antoine-Auffret <antoine@auffret.ovh>
2018-06-26 16:08:05 +0200

    test api list trajets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontOfficeComponent } from './front-office/front-office.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontOfficeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/front-office/front-office.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Headers, URLSearchParams } from '@angular/http';
+
+@Component({
+  selector: 'app-front-office',
+  templateUrl: './front-office.component.html',
+  styleUrls: ['./front-office.component.css']
+})
+
+@Injectable()
+export class FrontOfficeComponent {
+  constructor(private http: Http){}
+  valueRetour: any;
+  //methode appelée à chaque appel du composant
+  ngAfterViewInit(){
+    this.doGET();
+  }
+  // methode réalisant l'appel au web service et insérant la réponse
+  // dans une variable définie avant
+  doGET() {
+  console.log("GET");
+  let url = `http://localhost:8000/api/trajets`;
+  //this.http.get(url).subscribe(res => console.log(res.json()));
+  this.http.get(url).subscribe(res => this.valueRetour = res.json()['0']);
+  }
+}
